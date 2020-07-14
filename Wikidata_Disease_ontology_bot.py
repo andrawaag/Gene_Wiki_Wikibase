df71d45aff9484eacfccfa8eb0fb886c71209395
Andra Waagmeester <andra@micelio.be>
2020-07-14 20:50:19 +0200

    Wikidata bot for the disease ontology

--- Wikidata_Disease_ontology_bot.py
+++ Wikidata_Disease_ontology_bot.py
@@ -5,19 +5,17 @@ import pandas as pd
 from wikidataintegrator.wdi_helpers import try_write
 from datetime import datetime
 import copy
+import pprint
 
 
 def createDOReference(doid):
-    statedin = wdi_core.WDItemID("Q1", prop_nr="P6", is_reference=True)
+    statedin = wdi_core.WDItemID("Q5282129", prop_nr="P248", is_reference=True)
     retrieved = datetime.now()
     timeStringNow = retrieved.strftime("+%Y-%m-%dT00:00:00Z")
-    refRetrieved = wdi_core.WDTime(timeStringNow, prop_nr="P7", is_reference=True)
-    doid = wdi_core.WDExternalID(doid, prop_nr="P4", is_reference=True)
+    refRetrieved = wdi_core.WDTime(timeStringNow, prop_nr="P813", is_reference=True)
+    doid = wdi_core.WDExternalID(doid, prop_nr="P699", is_reference=True)
     return [statedin, refRetrieved, doid]
 
-
-# This code is to Login to Wikibase. The pattern of api is the URL of the wikibase + /w/api.php
-# In this example that is https://do-mondo.semscape.org/w/api.php
 
 print("Logging in...")
 if "WDUSER" in os.environ and "WDPASS" in os.environ:
@@ -26,7 +24,7 @@ if "WDUSER" in os.environ and "WDPASS" in os.environ:
 else:
     raise ValueError("WDUSER and WDPASS must be specified in local.py or as environment variables")
 
-wikibase = "https://do-mondo.semscape.org/w/api.php"
+wikibase = "https://www.wikidata.org/w/api.php"
 login = wdi_login.WDLogin(WDUSER, WDPASS,mediawiki_api_url=wikibase)
 
 ## The following section is to download the orginal Disease ontology
@@ -70,45 +68,61 @@ for row in qres:
       }, ignore_index=True)
 
 query = """
-  PREFIX wbt: <http://do-mondo.semscape.org/prop/direct/>
-  SELECT * WHERE {?item wbt:P3 ?exactMatch}
+  SELECT * WHERE {?item wdt:P2888 ?exactMatch ; wdt:P699 ?doid .}
 """
 existing_disease = dict()
-results = wdi_core.WDItemEngine.execute_sparql_query(query=query, endpoint="https://do-mondo.semscape.org/query/sparql" )
+results = wdi_core.WDItemEngine.execute_sparql_query(query=query)
 
 for result in results["results"]["bindings"]:
     existing_disease[str(result["exactMatch"]["value"])] = result["item"]["value"]
 
 for index, row in df_doNative.iterrows():
+
+    # Remove old statements
+    #old data
+    doid = row["doid"]
+    old_data_query = f"SELECT ?disease ?p (COUNT(?reference) AS ?references) WHERE \u007b ?disease wdt:P699 '{doid}' ; ?p ?node . ?node prov:wasDerivedFrom ?reference . ?reference pr:P699 '{doid}' .\u007d GROUP BY ?disease ?doid ?p "
+    results = wdi_core.WDItemEngine.execute_sparql_query(old_data_query)
+    pprint.pprint(results)
+    print(old_data_query)
+    delete_data = []
+    for result in results["results"]["bindings"]:
+        if result["references"]["value"] == 1:
+            print(result["p"]["value"])
+            delete_data.append(wdi_core.WDBaseDataType.delete_statement(prop_nr=result["p"]["value"].replace("http://www.wikidata.org/prop/", "")))
+    qid = existing_disease[row["do_uri"]].replace("http://www.wikidata.org/entity/", "")
+    wb_olddo_item = wdi_core.WDItemEngine(wd_item_id=qid, data=delete_data, keep_good_ref_statements=True)
+    try_write(wb_olddo_item, record_id=row["doid"], record_prop="P699", edit_summary="cleaned references a Disease Ontology",
+              login=login)
+
+
+    # Add recent disease ontology statements
     data = []
     do_reference = createDOReference(row["doid"])
     # disease ontology ID
-    data.append(wdi_core.WDExternalID(row["doid"], prop_nr="P4", references=[copy.deepcopy(do_reference)]))
+    data.append(wdi_core.WDExternalID(row["doid"], prop_nr="P699", references=[copy.deepcopy(do_reference)]))
 
     # disease ontology URI
-    data.append(wdi_core.WDUrl(row["do_uri"], prop_nr="P3", references=[copy.deepcopy(do_reference)]))
+    data.append(wdi_core.WDUrl(row["do_uri"], prop_nr="P2888", references=[copy.deepcopy(do_reference)]))
 
     # identifiers.org URI
-    data.append(wdi_core.WDUrl("http://identifiers.org/doid/"+row["doid"], prop_nr="P3", references=[copy.deepcopy(do_reference)]))
+    data.append(wdi_core.WDUrl("http://identifiers.org/doid/"+row["doid"], prop_nr="P2888", references=[copy.deepcopy(do_reference)]))
 
     # MESH
     if "MESH:" in row["exactMatch"]:
-        data.append(wdi_core.WDExternalID(row["exactMatch"], prop_nr="P8", references=[copy.deepcopy(do_reference)]))
+        data.append(wdi_core.WDExternalID(row["exactMatch"], prop_nr="P486", references=[copy.deepcopy(do_reference)]))
 
     if str(row["do_uri"]) in existing_disease.keys():
-        qid = existing_disease[row["do_uri"]].replace("http://do-mondo.semscape.org/entity/", "")
-        wb_do_item = wdi_core.WDItemEngine(wd_item_id=qid, data=data, mediawiki_api_url=wikibase,
-                                              sparql_endpoint_url="https://do-mondo.semscape.org/query/sparql",
-                                              keep_good_ref_statements=True)
+        qid = existing_disease[row["do_uri"]].replace("http://www.wikidata.org/entity/", "")
+        wb_do_item = wdi_core.WDItemEngine(wd_item_id=qid, data=data, keep_good_ref_statements=True)
     else:
-        wb_do_item = wdi_core.WDItemEngine(data=data, mediawiki_api_url=wikibase,
-                                              sparql_endpoint_url="https://do-mondo.semscape.org/query/sparql",
-                                              keep_good_ref_statements=True)
+        wb_do_item = wdi_core.WDItemEngine(data=data, keep_good_ref_statements=True)
 
     wb_do_item.set_label(row["label"], lang="en")
     wb_do_item.set_description("human disease", lang="en")
 
     if row["aliases"] != "":
         wb_do_item.set_aliases(row["aliases"].split("|"), lang="en")
-    try_write(wb_do_item, record_id=row["doid"], record_prop="P4", edit_summary="Updated a Disease Ontology",
+    try_write(wb_do_item, record_id=row["doid"], record_prop="P699", edit_summary="Updated a Disease Ontology",
               login=login)
+    sys.exit()
